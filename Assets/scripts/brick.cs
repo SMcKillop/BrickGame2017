6128c154a41ac9c243fd64ac1c617d5821668ed8
Unknown <mckillos1@hawkmail.newpaltz.edu>
2017-02-27 10:41:46 -0500

    Added unity files

---
+++ Assets/scripts/brick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class brick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/scripts/paddle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class paddle : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+    float speed = 5;
+	// Use this for initialization
+	void Start () {
+	    rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+        float x = Input.GetAxisRaw("Horizontal");
+        x = x * speed;
+        rigidBody.velocity = new Vector2(x, 0);
+	}
+}
