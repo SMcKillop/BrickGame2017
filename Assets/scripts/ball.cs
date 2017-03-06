7208246c6dd171949edbd2fc44f4571bd20aae59
Unknown <mckillos1@hawkmail.newpaltz.edu>
2017-03-06 10:47:32 -0500

    GameOver and lives set up

--- Assets/scripts/ball.cs
+++ Assets/scripts/ball.cs
@@ -4,22 +4,41 @@ using System.Collections;
 public class ball : MonoBehaviour {
 
    public Vector2 startingVelocity = new Vector2(5, -20);
+    private Vector3 startingPosition;
+    public GameObject gameOver;
+
+    int lives = 3; 
 
     // Use this for initialization
     void Start () {
+        startingPosition = transform.position;
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if(transform.position.y < -3.34f)
+	    if(transform.position.y < -3.5f)
         {
             GetOut();
         }
-	}
+        if(Input.GetButtonDown("Jump")) {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
+    }
 
-    void GetOut()
+        void GetOut();
     {
         Debug.Log("You are out");
+        lives = lives - 1;
+
+        transform.position = startingPosition;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+        if (lives == 0)
+        {
+            DoGameOver();
+        }    
     }
+
+    void DoGameOver() {
+
+        gameOver.SetActive(true);}
 }
